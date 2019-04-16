a1ba4e84b10384e23097fc57ba78bdf57d4ef494
aishwariya2012 <bhattaishwariya@gmail.com>
2019-04-16 11:00:51 -0700

    ARIMA Model

--- Project.py
+++ Project.py
@@ -1,6 +1,35 @@
 import pandas as pd
-df = pd.read_csv('C:\\Users\\Aishwariya\\Desktop\\CMPE256 Project\\test.csv')
+from pandas import read_csv
+from pandas import datetime
+from statsmodels.tsa.arima_model import ARIMA
+from pandas import DataFrame
+from matplotlib import pyplot
+#from pandas.tools.plotting import autocorrelation_plot
+df = pd.read_csv('C:\Users\Aishwariya\Desktop\CMPE256 Project\test.csv')
 df
 
 df.fillna(method='ffill', inplace=True)
 df
+
+ 
+def parser(x):
+	return pd.to_datetime(x, unit='s')
+ 
+series = read_csv('C:\\Users\\Aishwariya\\Desktop\\CMPE256 Project\\test.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series.fillna(method='ffill', inplace=True)
+series
+#autocorrelation_plot(series)
+#pyplot.show()
+series1 = series.iloc[:,[6]]
+series1
+
+model = ARIMA(series1, order=(5,1,0))
+model_fit = model.fit(disp=0)
+print(model_fit.summary())
+# plot residual errors
+residuals = DataFrame(model_fit.resid)
+residuals.plot()
+pyplot.show()
+residuals.plot(kind='kde')
+pyplot.show()
+print(residuals.describe())
